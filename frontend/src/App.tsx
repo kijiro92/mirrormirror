836c65dc4b609f435475051d30bce4b105454709
MaiTai Steve <kijiro.92@mac.com>
2021-03-08 23:05:44 -0500

    Added time and date. Button to clear and populate screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,82 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './WeatherLogo.png';
 import './App.css';
 
-function App() {
+const App = () => {
 
-  const [weather, setWeather] = useState("--º");
-  function setTheState (body: string): void {
-    setWeather(body);
-  }
-  function readTheResponse (response: Response): void {
-    response.text().then(setTheState).catch();
-  }
-  let promise = fetch("/weather")
-  promise.then(readTheResponse).catch();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className={"Weather"}>
-          <div className={"WeatherLogoContainer"}>
-            <img src={logo} className="WeatherLogo" alt="logo" />
-          </div>
-          <div className={"WeatherText"}>
-            {weather+"º"}
-          </div>
+    const [weather, setWeather] = useState("--º");
+    const [screenIsClear, setScreenIsClear] = useState(false);
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date());
+        }, 500);
+        return () => {
+            clearInterval(timer);
+        }
+    }, []);
+    const readTheResponse = (response: Response): void => {
+        response.text().then((body: string): void => {
+            setWeather(body);
+        }).catch();
+    }
+    const clearScreen = () => {
+        console.log("hello");
+        setScreenIsClear(true);
+    }
+    const onScreen = (): void => {
+        console.log("Byee");
+        setScreenIsClear(false);
+    }
+
+    // function name(param) {
+    //
+    // }
+    // is the exact same thing as
+    // const name = (param) => {
+    //
+    // }
+
+    // We gotta learn conditionals
+    // true || false
+    // false && true
+    // false || true
+    // true && false
+
+    let promise = fetch("/weather")
+    promise.then(readTheResponse).catch();
+    return (
+        <div className="App">
+            <header className="App-header">
+                {!screenIsClear && <>
+                            <div className={"Weather"}>
+                                <div className={"WeatherLogoContainer"}>
+                                    <img src={logo} className="WeatherLogo" alt="logo"/>
+                                </div>
+                                <div className={"WeatherText"}>
+                                    {weather + "º"}
+                                </div>
+                                <div className={"Time"}>
+                                    {time.toLocaleString()}
+                                </div>
+                            </div>
+                            <div className="Button">
+                                <button onClick={clearScreen}>
+                                    Clear screen
+                                </button>
+                            </div>
+                        </>
+                }
+                {screenIsClear && <div className="Button">
+                            <button onClick={onScreen}>
+                               On SCreen
+                            </button>
+                        </div>
+                    }
+            </header>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
