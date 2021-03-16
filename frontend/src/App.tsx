09e72847f44ae48b2ee3d762ee3546e2d13368ed
MaiTai Steve <kijiro.92@mac.com>
2021-03-15 23:42:46 -0400

    Added compliments and a compliment button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ const App = () => {
     const [weather, setWeather] = useState("--º");
     const [screenIsClear, setScreenIsClear] = useState(false);
     const [time, setTime] = useState(new Date());
+    const [compliment, setCompliment] = useState(false);
 
     useEffect(() => {
         const timer = setInterval(() => {
@@ -16,15 +17,27 @@ const App = () => {
             clearInterval(timer);
         }
     }, []);
+
     const readTheResponse = (response: Response): void => {
         response.text().then((body: string): void => {
             setWeather(body);
         }).catch();
     }
+
     const clearScreen = () => {
         console.log("hello");
         setScreenIsClear(true);
     }
+
+    const complimentMe = () => {
+        setCompliment(true);
+        setComplimentToFalseAfter5Seconds();
+    }
+
+    const setComplimentToFalseAfter5Seconds =() => {
+        setTimeout(() => setCompliment(false), 5000);
+    }
+
     const onScreen = (): void => {
         console.log("Byee");
         setScreenIsClear(false);
@@ -50,30 +63,41 @@ const App = () => {
         <div className="App">
             <header className="App-header">
                 {!screenIsClear && <>
-                            <div className={"Weather"}>
-                                <div className={"WeatherLogoContainer"}>
-                                    <img src={logo} className="WeatherLogo" alt="logo"/>
-                                </div>
-                                <div className={"WeatherText"}>
-                                    {weather + "º"}
-                                </div>
-                                <div className={"Time"}>
-                                    {time.toLocaleString()}
-                                </div>
-                            </div>
-                            <div className="Button">
-                                <button onClick={clearScreen}>
-                                    Clear screen
-                                </button>
-                            </div>
-                        </>
+                    <div className={"Weather"}>
+                        <div className={"WeatherLogoContainer"}>
+                            <img src={logo} className="WeatherLogo" alt="logo"/>
+                        </div>
+                        <div className={"WeatherText"}>
+                            {weather}º
+                        </div>
+                        <div className={"ComplimentText"}>
+                            {compliment && <>
+                                You look great 2day!
+                            </>
+                            }
+                        </div>
+                        <div className={"Time"}>
+                            {time.toLocaleString()}
+                        </div>
+                    </div>
+                    <div className="Button">
+                        <button onClick={complimentMe}>
+                            How do I look?
+                        </button>
+                    </div>
+                    <div className="Button">
+                        <button onClick={clearScreen}>
+                            Clear screen
+                        </button>
+                    </div>
+                </>
                 }
                 {screenIsClear && <div className="Button">
-                            <button onClick={onScreen}>
-                               On SCreen
-                            </button>
-                        </div>
-                    }
+                    <button onClick={onScreen}>
+                        On SCreen
+                    </button>
+                </div>
+                }
             </header>
         </div>
     );
